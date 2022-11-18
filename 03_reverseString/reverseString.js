748ef02155577812a8339f734447f9a42580db81
Neil H <neilhemm0@gmail.com>
2022-11-17 21:04:42 -0500

    Complete all tests for js exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const wordArray = word.split("");
+    let reversed = "";
 
+    for (let i = wordArray.length -1; i >= 0; i--) {
+        reversed += wordArray[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line

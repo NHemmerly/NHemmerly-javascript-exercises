aef502f0c93ac9f15fdd3157706d68ac7e002443
Neil H <neilhemm0@gmail.com>
2022-11-18 20:21:20 -0500

    Complete javascript exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...restArgs) {
+    if (!(arguments.length > 1)) {
+        return list;
+    }
 
+    let newArray = new Array();
+    for (const num of list) {
+        if (!(restArgs.includes(num))) {
+            newArray = newArray.concat(num);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line

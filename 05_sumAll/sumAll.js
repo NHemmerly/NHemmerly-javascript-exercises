a3a96234a4da55627e71b7d88589013a1a82eafe
Neil H <neilhemm0@gmail.com>
2022-11-18 21:01:53 -0500

    Complete javascript exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a < 0 || b < 0) || (typeof a && typeof b) != "number") {
+        return 'ERROR';
+    }
+    if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
 
+    let sum = ((b - a) + 1) / 2 * (a + b);
+
+    return sum;
 };
 
 // Do not edit below this line
